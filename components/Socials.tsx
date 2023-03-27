4085044b49e2bdce5ed0619209e02f82b2f20866
kelalm <34616059+kelalm@users.noreply.github.com>
2023-03-26 23:44:32 -0400

    added social icons buttons with icons

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,7 +1,39 @@
+import { Group, Button, rem } from "@mantine/core";
+import { IconBrandGithubFilled, IconMail } from "@tabler/icons-react";
+
 export default function Socials() {
   return (
-    <>
-      <h1>GitHub Email Twitter</h1>
-    </>
+    <Group position="center">
+      <Button
+        component="a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/kelalm"
+        leftIcon={<IconBrandGithubFilled />}
+        styles={(theme) => ({
+          root: {
+            border: 0,
+            height: rem(42),
+            paddingLeft: rem(20),
+            paddingRight: rem(10),
+          },
+        })}
+      ></Button>
+      <Button
+        component="a"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="mailto:kelvinalmonte@live.com"
+        leftIcon={<IconMail />}
+        styles={(theme) => ({
+          root: {
+            border: 0,
+            height: rem(42),
+            paddingLeft: rem(20),
+            paddingRight: rem(10),
+          },
+        })}
+      ></Button>
+    </Group>
   );
 }
